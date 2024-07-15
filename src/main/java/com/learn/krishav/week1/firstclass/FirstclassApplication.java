fbd1c51b34e08c24e3a3d93b7f90a0c4c9ad00c9
Krishav Sharma <krishav@Krishavs-Air.lan>
2024-07-16 02:31:52 +0530

    initial commit

---
+++ src/main/java/com/learn/krishav/week1/firstclass/FirstclassApplication.java
@@ -0,0 +1,13 @@
+package com.learn.krishav.week1.firstclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/krishav/week1/firstclass/FirstclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.krishav.week1.firstclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
